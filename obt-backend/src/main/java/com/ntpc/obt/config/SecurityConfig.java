35d3ce57fe43941042d926e6d3312edc56e8517b
Abhay Kumar Tiwari <90232441+Creater8@users.noreply.github.com>
2026-09-03 12:12:25 +0530

    Fix allowed origins in SecurityConfig

--- obt-backend/src/main/java/com/ntpc/obt/config/SecurityConfig.java
+++ obt-backend/src/main/java/com/ntpc/obt/config/SecurityConfig.java
@@ -267,7 +267,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:5500",
-                        "http://127.0.0.1:5500"
+                        "http://127.0.0.1:5500",
                         "https://obt-agency-management-final-structure.onrender.com"
                 )
         );
