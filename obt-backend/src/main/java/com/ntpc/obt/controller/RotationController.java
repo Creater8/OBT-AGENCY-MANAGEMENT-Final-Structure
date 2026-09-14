00f6c16bab193fcd92c025ed2a1979df1403c08b
Abhay Kumar Tiwari <90232441+Creater8@users.noreply.github.com>
2026-09-14 21:32:06 +0530

    Fix comment formatting in RotationController

--- obt-backend/src/main/java/com/ntpc/obt/controller/RotationController.java
+++ obt-backend/src/main/java/com/ntpc/obt/controller/RotationController.java
@@ -220,7 +220,7 @@ public class RotationController {
      *
      * 3. If there is no future Agency:
      *
-     *    Restart from the first Agency according to
+     *    restart from the first Agency according to
      *    rotationOrder.
      *
      * API:
